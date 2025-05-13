15d23e8682bcf36e5174ff58feab3ec606083639
김민규 <jun3021303@naver.com>
2025-05-13 17:40:37 +0900

    chatgpt 연결

--- zipkok_spring_gpt/src/main/java/com/ssafy/house/ai/controller/AIChatController.java
+++ zipkok_spring_gpt/src/main/java/com/ssafy/house/ai/controller/AIChatController.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 import org.springframework.ai.chat.client.ChatClient.AdvisorSpec;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/ai")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "http://localhost:5173")
 public class AIChatController implements RestControllerHelper {
 
     private final AiChatService chatService;
