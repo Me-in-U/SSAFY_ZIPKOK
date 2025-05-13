4330611e6b50b4e8d340c0b1ac696ecc1035663b
김민규(KIM MINGYU) <jun3021303@naver.com>
2025-05-14 06:03:03 +0900

    mini pc에 업로드

--- zipkok_spring_gpt/src/main/java/com/ssafy/house/ZipkokApplication.java
+++ zipkok_spring_gpt/src/main/java/com/ssafy/house/ZipkokApplication.java
@@ -3,9 +3,17 @@ package com.ssafy.house;
 import org.springframework.ai.autoconfigure.vectorstore.redis.RedisVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = RedisVectorStoreAutoConfiguration.class)
-public class ZipkokApplication {
+public class ZipkokApplication extends SpringBootServletInitializer {
+
+	// WAR 배포 시 톰캣이 호출하는 메서드
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ZipkokApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkokApplication.class, args);
