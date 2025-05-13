28543e038bac4955f925a2f11da05da3332d332b
김민규 <jun3021303@naver.com>
2025-05-13 17:26:49 +0900

    필요없는거 제거

--- zipkok_spring_gpt/src/main/java/com/ssafy/house/model/dto/ChatMessage.java
+++
@@ -1,9 +0,0 @@
-// package com.ssafy.house.model.dto;
-
-// import lombok.Data;
-
-// @Data
-// public class ChatMessage {
-// private String role; // "user" 또는 "system"
-// private String content;
-// }
