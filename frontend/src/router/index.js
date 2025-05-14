f858548375fbdcbd00e1fb4e0c1a63f8aa913882
solar <64721658+jung980514@users.noreply.github.com>
2025-05-14 09:43:06 +0900

    로그인 폼 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHireplace-with-user-password } from 'vue-router'
-
+import LoginForm from '../components/LoginForm.vue'
 const router = createRouter({
   hireplace-with-user-password: createWebHireplace-with-user-password(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+      path: "/",
+      name: "home",
+      component: () => import("../App.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginForm,
+    },
+  ],
 })
 
 export default router
