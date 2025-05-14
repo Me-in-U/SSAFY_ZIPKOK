de6b6b87f4e32f308db61080718898abffc9084e
김민규 <jun3021303@naver.com>
2025-05-14 17:39:53 +0900

    Cors + 정리

--- backend/src/main/java/com/ssafy/house/ai/service/BasicAiChatService.java
+++ backend/src/main/java/com/ssafy/house/ai/service/BasicAiChatService.java
@@ -14,50 +14,50 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class BasicAiChatService implements AiChatService {
-    @Qualifier("advisedChatClient")
-    private final ChatClient advisedChatClient;
-    private final DateTimeTools dateTimeTools;
-    private final MemberTools memberTools;
+        @Qualifier("advisedChatClient")
+        private final ChatClient advisedChatClient;
+        private final DateTimeTools dateTimeTools;
+        private final MemberTools memberTools;
 
-    @Override
-    public String timeToolGeneration(String userInput) {
-        return this.advisedChatClient
-                .prompt()
-                .system(c -> c.param("language", "Korean")
-                        .param("character", "Chill한"))
-                .user(userInput)
-                .tools(dateTimeTools)
-                .call()
-                .content();
-    }
+        @Override
+        public String timeToolGeneration(String userInput) {
+                return this.advisedChatClient
+                                .prompt()
+                                .system(c -> c.param("language", "Korean")
+                                                .param("character", "Chill한"))
+                                .user(userInput)
+                                .tools(dateTimeTools)
+                                .call()
+                                .content();
+        }
 
-    @Override
-    public String memberToolGeneration(String userInput) {
-        return this.advisedChatClient
-                .prompt()
-                .system(c -> c.param("language", "Korean")
-                        .param("character", "Chill한"))
-                .user(userInput)
-                .tools(memberTools)
-                .call()
-                .content();
-    }
+        @Override
+        public String memberToolGeneration(String userInput) {
+                return this.advisedChatClient
+                                .prompt()
+                                .system(c -> c.param("language", "Korean")
+                                                .param("character", "Chill한"))
+                                .user(userInput)
+                                .tools(memberTools)
+                                .call()
+                                .content();
+        }
 
-    // private final ChatClient ragChatDefaultClient;
-    // private final ChatClient ragChatCustomClient;
+        // private final ChatClient ragChatDefaultClient;
+        // private final ChatClient ragChatCustomClient;
 
-    // @Override
-    // public String ragGeneration(String userInput, boolean contextOnly,
-    // Consumer<AdvisorSpec> advisorSpec) {
-    // ChatClient chatClient = contextOnly ? ragChatDefaultClient :
-    // ragChatCustomClient;
-    // var spec = chatClient.prompt()
-    // .system(c -> c.param("language", "Korean").param("character", "Chill한"))
-    // .user(userInput);
-    // // 런타임에 filter 표현식에 동적 filtering
-    // if (advisorSpec != null) {
-    // spec.advisors(advisorSpec);
-    // }
-    // return spec.call().content();
-    // }
+        // @Override
+        // public String ragGeneration(String userInput, boolean contextOnly,
+        // Consumer<AdvisorSpec> advisorSpec) {
+        // ChatClient chatClient = contextOnly ? ragChatDefaultClient :
+        // ragChatCustomClient;
+        // var spec = chatClient.prompt()
+        // .system(c -> c.param("language", "Korean").param("character", "Chill한"))
+        // .user(userInput);
+        // // 런타임에 filter 표현식에 동적 filtering
+        // if (advisorSpec != null) {
+        // spec.advisors(advisorSpec);
+        // }
+        // return spec.call().content();
+        // }
 }
