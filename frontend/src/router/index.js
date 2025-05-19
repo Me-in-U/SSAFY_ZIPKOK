b9a0d08ea2d242ff78fa56fd806461ea38e6b89e
김민규 <jun3021303@naver.com>
2025-05-19 11:56:52 +0900

    루트 구조 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,32 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginForm from '../components/LoginForm.vue'
-const router = createRouter({
+import AppLayout from '@/views/AppLayout.vue'
+import HomeContent from '@/views/HomeContent.vue'
+import LoginForm from '@/components/LoginForm.vue'
+import RegisterForm from '@/components/RegisterForm.vue'
+import RecommendedPropertiesModal from '@/components/RecommendedPropertiesModal.vue'
+
+export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-      path: "/",
-      name: "home",
-      component: () => import("../App.vue"),
-    },
+  routes: [
     {
-      path: "/login",
-      name: "login",
-      component: LoginForm,
+      path: '/',
+      component: AppLayout,
+      children: [
+        { path: '', component: HomeContent },
+        {
+          path: 'login',
+          components: { default: HomeContent, modal: LoginForm },
+        },
+        {
+          path: 'register',
+          components: { default: HomeContent, modal: RegisterForm },
+        },
+        {
+          path: 'recommended',
+          components: { default: HomeContent, modal: RecommendedPropertiesModal },
+        },
+      ],
     },
   ],
 })
-
-export default router
